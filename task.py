6ea7fce66ad0ec0922a15b57729d77d898036cf9
Rodger FRANCIS <rfrancis@e4r16p9.wethinkcode.co.za>
2017-11-17 14:30:30 +0200

    task.py

--- task.py
+++ task.py
@@ -8,28 +8,54 @@ import threading
 class Task:
 	def __init__(self, dic):
 		self.attr = dic
-		self.prog = dic['prog']
-		for i in dic:
-			self.i = (dic[i]) 
-	def run(self):
-		proc = subprocess.Popen([self.prog],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-		stdout_value = proc.communicate()
-		print ("stdout:", (stdout_value[:len(stdout_value)-1]) , proc.returncode)
-	
-	
+		self.proc = None 
+	def start(self):
+		rerun = True
+		while self.attr['NUM'] > 0:
+			self.proc = subprocess.Popen([self.attr['CMD']],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,cwd =self.attr['DIR'])
+			self.attr['NUM'] -= 1
+		t = [self.proc] * self.attr['NUM']
+		STDOUT_value = str(self.proc.communicate()[0])
+		while rerun and self.attr["RESTART_NUM"] >= 0:
+			if	self.attr["STDOUT"] == "STDOUT":
+				print ("STDOUT:", (STDOUT_value[:len(STDOUT_value)-1]))
+			else :
+				fi = open(self.attr["STDOUT"], "w+")
+				fi.write(STDOUT_value)
+				fi.close()
+			for i in t:
+				if i.returncode != self.attr["RET_CODE"]:
+					i = subprocess.Popen([self.attr['CMD']],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE ,cwd = self.attr['DIR'])
+			self.attr["RESTART_NUM"] -= 1
+					
 
 class TaskManager:
 	def __init__(self , processes):
-			self.processes = processes
-	def run(self):
-		threads = []
+		self.processes = processes
+		self.threadList = []
 		for i in self.processes:
 			if i.attr["autostart"]:
-				t = threading.Thread(target=i.run())
-				threads.append(t)
-				t.start()
-	
+				self.threadList = threading.Thread(target=i.start())
+				self.threads.append(threadList)
+				self.threadList.start()
+	def status():
+		for i in self.threadList:
+			if self.threadList.poll():
+				print ("alive",i.proc.pid) 
+			else:
+				print ("dead",i.proc,pid)
+	def start(pname):
+		for j in self.threadList:
+			if j.attr['CMD'] == pname:
+				j.start()
+	def end(pname):
+		for j in self.threadList:
+			if j.attr['CMD'] == pname:
+				j.proc.terminate()
+
 
 if __name__ == '__main__':
-	a=  TaskManager([Task({"prog" :"ls" ,"autostart" : True }) ,Task({"prog" :"$?", "autostart" : False }) ,Task({"prog" :"pwd","autostart" : True })])
-	a.run()
+	Task({"CMD" : "echo -n 'l'", "STDOUT" : "STDOUT", "NUM" : 10 , "RET_CODE" : 3, "RESTART_NUM" : 2 , "DIR" : "." }).start()
+	pass
+
+
